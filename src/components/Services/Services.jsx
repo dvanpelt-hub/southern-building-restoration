45267d8b1adf4146ec04fe4604ca79c96f6e5cb5
dvanpelt-hub <vanpeltdh1@gmail.com>
2025-05-20 13:20:28 +0100

    updated window content

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -28,21 +28,21 @@ const Services = () => {
             methods to ensure the aesthetic and functional performance of each
             restoration project. Whether it’s refreshing the finish on a period
             property or undertaking more extensive lime plastering works, SBR
-            delivers meticulous and long-lasting results. 
+            delivers meticulous and long-lasting results.
             <br />
             <br />
-            In addition to
-            traditional plastering, SBR’s lime rendering services play a vital
-            role in the conservation of historical facades. Lime render provides
-            a durable, weather-resistant coating that allows moisture to escape
-            from the building, protecting the underlying structure from damp and
-            decay. The team at SBR is highly experienced in applying lime render
-            to a wide range of surfaces, including stone, brick, and timber.
-            Their careful approach ensures that the character of the building is
-            preserved while enhancing its longevity. From delicate internal lime
-            plastering to robust external rendering, SBR offers a full range of
-            lime-based services, providing expert solutions to maintain and
-            restore the beauty of historic properties for future generations.
+            In addition to traditional plastering, SBR’s lime rendering services
+            play a vital role in the conservation of historical facades. Lime
+            render provides a durable, weather-resistant coating that allows
+            moisture to escape from the building, protecting the underlying
+            structure from damp and decay. The team at SBR is highly experienced
+            in applying lime render to a wide range of surfaces, including
+            stone, brick, and timber. Their careful approach ensures that the
+            character of the building is preserved while enhancing its
+            longevity. From delicate internal lime plastering to robust external
+            rendering, SBR offers a full range of lime-based services, providing
+            expert solutions to maintain and restore the beauty of historic
+            properties for future generations.
           </p>
         </div>
       </div>
@@ -56,10 +56,31 @@ const Services = () => {
         <div className="services-content-timber">
           <h3>Lime pointing</h3>
           <p>
-            SBR specializes in all aspects of lime pointing, offering expert services designed to preserve and protect the integrity of historic buildings and structures. Lime pointing is a traditional technique used to repoint mortar joints in brick and stonework, and SBR excels in this highly skilled process. Unlike modern cement-based mortar, lime mortar is breathable, flexible, and much more sympathetic to the natural materials of older buildings. SBR’s team understands the importance of using the right mix and application techniques to match the original material, ensuring the longevity and structural stability of period properties. Whether it’s repointing a building’s facade or repairing joints on smaller features, SBR provides a professional and precise service that respects the craftsmanship of the past.
+            SBR specializes in all aspects of lime pointing, offering expert
+            services designed to preserve and protect the integrity of historic
+            buildings and structures. Lime pointing is a traditional technique
+            used to repoint mortar joints in brick and stonework, and SBR excels
+            in this highly skilled process. Unlike modern cement-based mortar,
+            lime mortar is breathable, flexible, and much more sympathetic to
+            the natural materials of older buildings. SBR’s team understands the
+            importance of using the right mix and application techniques to
+            match the original material, ensuring the longevity and structural
+            stability of period properties. Whether it’s repointing a building’s
+            facade or repairing joints on smaller features, SBR provides a
+            professional and precise service that respects the craftsmanship of
+            the past.
             <br />
             <br />
-            With a deep understanding of historic masonry, SBR’s lime pointing services not only improve the aesthetic appeal of a building but also enhance its performance. Lime mortar allows moisture to escape, preventing the trapped moisture that can cause damage in modern cement-based systems. SBR uses the best quality lime products and employs traditional methods to ensure that each project is completed to the highest standards. From minor repointing repairs to larger conservation projects, SBR’s lime pointing services are an essential part of maintaining and restoring the beauty and function of historic properties for years to come.
+            With a deep understanding of historic masonry, SBR’s lime pointing
+            services not only improve the aesthetic appeal of a building but
+            also enhance its performance. Lime mortar allows moisture to escape,
+            preventing the trapped moisture that can cause damage in modern
+            cement-based systems. SBR uses the best quality lime products and
+            employs traditional methods to ensure that each project is completed
+            to the highest standards. From minor repointing repairs to larger
+            conservation projects, SBR’s lime pointing services are an essential
+            part of maintaining and restoring the beauty and function of
+            historic properties for years to come.
           </p>
         </div>
       </div>
@@ -73,14 +94,37 @@ const Services = () => {
         <div className="services-content-plastering">
           <h3>Brickwork and Masonry</h3>
           <p>
-            SBR offers a complete range of brickwork, stone, and general masonry repair services, specializing in the restoration and preservation of period properties. With an expert understanding of the intricacies involved in working with historic buildings, SBR’s team is skilled in repairing and conserving traditional brick and stonework, ensuring that the architectural integrity and aesthetic of these properties are maintained. Whether it’s repointing weathered brickwork, repairing damaged stone facades, or restoring intricate masonry features, SBR applies both time-honored techniques and modern methods to deliver high-quality results. Their approach is always mindful of the building’s original character, using materials that match the historical craftsmanship and ensuring that every repair blends seamlessly with the existing structure.
-            <br/>
-            <br/>
-            In addition to brick and stone repairs, SBR provides a wide array of masonry services for period properties, including lintel and arch repairs, chimney restoration, and the restoration of ornamental stonework. The company’s attention to detail and respect for the history of each building makes them a trusted partner for those looking to preserve the charm and heritage of their property. By utilizing skilled craftsmen and carefully sourced materials, SBR ensures that each project is completed with precision and care, extending the life of the property while maintaining its historical value. Whether dealing with weathered stone or deteriorating brickwork, SBR’s masonry services are designed to protect and enhance period properties for generations to come.
+            SBR offers a complete range of brickwork, stone, and general masonry
+            repair services, specializing in the restoration and preservation of
+            period properties. With an expert understanding of the intricacies
+            involved in working with historic buildings, SBR’s team is skilled
+            in repairing and conserving traditional brick and stonework,
+            ensuring that the architectural integrity and aesthetic of these
+            properties are maintained. Whether it’s repointing weathered
+            brickwork, repairing damaged stone facades, or restoring intricate
+            masonry features, SBR applies both time-honored techniques and
+            modern methods to deliver high-quality results. Their approach is
+            always mindful of the building’s original character, using materials
+            that match the historical craftsmanship and ensuring that every
+            repair blends seamlessly with the existing structure.
+            <br />
+            <br />
+            In addition to brick and stone repairs, SBR provides a wide array of
+            masonry services for period properties, including lintel and arch
+            repairs, chimney restoration, and the restoration of ornamental
+            stonework. The company’s attention to detail and respect for the
+            history of each building makes them a trusted partner for those
+            looking to preserve the charm and heritage of their property. By
+            utilizing skilled craftsmen and carefully sourced materials, SBR
+            ensures that each project is completed with precision and care,
+            extending the life of the property while maintaining its historical
+            value. Whether dealing with weathered stone or deteriorating
+            brickwork, SBR’s masonry services are designed to protect and
+            enhance period properties for generations to come.
           </p>
         </div>
       </div>
-        <div className="services-container-reverse">
+      <div className="services-container-reverse">
         <div className="services-image-container">
           <span
             className="services-image"
@@ -90,10 +134,49 @@ const Services = () => {
         <div className="services-content-timber">
           <h3>Timber</h3>
           <p>
-            SBR is a leading provider in the field of timber restoration and conservation, offering a comprehensive range of services that address all aspects of timber care and preservation. With a deep understanding of the unique qualities and challenges that timber presents, SBR specializes in restoring and conserving wooden structures, flooring, and furniture, ensuring that they not only regain their original beauty but also remain durable for years to come. Their expert team uses state-of-the-art techniques and eco-friendly solutions, offering services such as timber cleaning, refinishing, structural repairs, and protection against future wear and decay. Whether it’s historical restoration or modern timber treatments, SBR ensures every project is handled with the utmost care and attention to detail.
+            SBR is a leading provider in the field of timber restoration and
+            conservation, offering a comprehensive range of services that
+            address all aspects of timber care and preservation. With a deep
+            understanding of the unique qualities and challenges that timber
+            presents, SBR specializes in restoring and conserving wooden
+            structures, flooring, and furniture, ensuring that they not only
+            regain their original beauty but also remain durable for years to
+            come. Their expert team uses state-of-the-art techniques and
+            eco-friendly solutions, offering services such as timber cleaning,
+            refinishing, structural repairs, and protection against future wear
+            and decay. Whether it’s historical restoration or modern timber
+            treatments, SBR ensures every project is handled with the utmost
+            care and attention to detail.
             <br />
             <br />
-            The company prides itself on a holistic approach to timber conservation, combining traditional methods with innovative technology to deliver sustainable and long-lasting results. SBR’s timber restoration services extend to a variety of applications, from residential and commercial properties to heritage sites and conservation projects. Their extensive knowledge of different types of wood and their behavior in different environments allows them to provide tailored solutions that meet the specific needs of each client. By preserving the natural beauty and strength of timber, SBR helps to extend the life of valuable wooden assets, while also enhancing their aesthetic appeal.
+            The company prides itself on a holistic approach to timber
+            conservation, combining traditional methods with innovative
+            technology to deliver sustainable and long-lasting results. SBR’s
+            timber restoration services extend to a variety of applications,
+            from residential and commercial properties to heritage sites and
+            conservation projects. Their extensive knowledge of different types
+            of wood and their behavior in different environments allows them to
+            provide tailored solutions that meet the specific needs of each
+            client. By preserving the natural beauty and strength of timber, SBR
+            helps to extend the life of valuable wooden assets, while also
+            enhancing their aesthetic appeal.
+          </p>
+        </div>
+      </div>
+        <div className="services-container">
+        <div className="services-image-container">
+          <span
+            className="services-image"
+            alt="A picture of a historical chimney."
+          />
+        </div>
+        <div className="services-content-window">
+          <h3>Window Restoration</h3>
+          <p>
+            SBR offers expert window restoration services, specializing in the preservation and restoration of period windows. These windows, often found in historic or period properties, are integral to the character and charm of the building, but over time, they can deteriorate due to age, weather exposure, and general wear. SBR’s team of skilled craftsmen is experienced in restoring original windows to their former glory, using traditional methods and high-quality materials. They carefully assess the condition of each window and employ specialized techniques to repair, reglaze, and refurbish frames, sashes, and glazing, ensuring that the aesthetic and historical value of the property is preserved.
+            <br />
+            <br />
+            In addition to enhancing the appearance and functionality of period windows, SBR’s restoration services improve energy efficiency, offering clients the opportunity to upgrade their windows without compromising the heritage of the building. By repairing and restoring original windows, SBR helps reduce waste and maintain the authenticity of a property, while also addressing modern performance standards. Whether it’s replacing damaged timber, restoring leaded glass, or reworking window mechanisms for smoother operation, SBR’s restoration process ensures that period windows not only look beautiful but also operate efficiently and sustainably for years to come.
           </p>
         </div>
       </div>
