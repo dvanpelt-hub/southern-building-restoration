6dbf90eb339a6d313681fe33191de8f961983b00
dvanpelt-hub <vanpeltdh1@gmail.com>
2025-05-22 12:08:38 +0100

    updated gallery

--- src/media/slides.jsx
+++ src/media/slides.jsx
@@ -1,40 +1,143 @@
-import image2 from "../media/chimney.jpg";
+import image1 from "../media/house3.png";
+import image2 from "../media/bricksteps.JPG";
+import image3 from "../media/chimney2.jpg";
+import image4 from "../media/chimneyrender.jpg";
+import image5 from "../media/house.png";
+import image6 from "../media/house2.png";
+import image7 from "../media/house3.png";
+import image8 from "../media/insideroom.jpg";
+import image9 from "../media/interior.png";
+import image10 from "../media/interior2.JPG";
+import image11 from "../media/interior2.png";
+import image12 from "../media/limeplastering.jpg";
+import image13 from "../media/limeplastering2.jpg";
+import image14 from "../media/outside.JPG";
+import image15 from "../media/repoint.jpg";
+import image16 from "../media/roof.jpg";
+import image17 from "../media/roof2.jpg";
+import image18 from "../media/scaffolding.JPG";
+import image19 from "../media/smallhouse.JPG";
+import image20 from "../media/smallhouse2.png";
+import image21 from "../media/walloutside.jpg";
+import image22 from "../media/walloutside2.jpg";
+import image23 from "../media/window.jpg";
+import image24 from "../media/window2.jpg";
+import image25 from "../media/brickbpointing.jpg"
+import image26 from "../media/bagshotlea.jpg"
+import image27 from "../media/bagshotlea2.jpg"
+import image28 from "../media/bagshotlea3.jpg"
 
 export const slides = [
   {
-    thumbnail: image2,
-    full: image2,
+    thumbnail: image1,
+    full: image1,
   },
   {
     thumbnail: image2,
     full: image2,
   },
   {
-    thumbnail: image2,
-    full: image2,
+    thumbnail: image3,
+    full: image3,
   },
   {
-    thumbnail: image2,
-    full: image2,
+    thumbnail: image4,
+    full: image4,
   },
   {
-    thumbnail: image2,
-    full: image2,
+    thumbnail: image5,
+    full: image5,
   },
   {
-    thumbnail: image2,
-    full: image2,
+    thumbnail: image6,
+    full: image6,
   },
   {
-    thumbnail: image2,
-    full: image2,
+    thumbnail: image7,
+    full: image7,
   },
   {
-    thumbnail: image2,
-    full: image2,
+    thumbnail: image8,
+    full: image8,
   },
   {
-    thumbnail: image2,
-    full: image2,
+    thumbnail: image9,
+    full: image9,
+  },
+  {
+    thumbnail: image10,
+    full: image10,
+  },
+  {
+    thumbnail: image11,
+    full: image11,
+  },
+  {
+    thumbnail: image12,
+    full: image12,
+  },
+  {
+    thumbnail: image13,
+    full: image13,
+  },
+  {
+    thumbnail: image14,
+    full: image14,
+  },
+  {
+    thumbnail: image15,
+    full: image15,
+  },
+  {
+    thumbnail: image16,
+    full: image16,
+  },
+  {
+    thumbnail: image17,
+    full: image17,
+  },
+  {
+    thumbnail: image18,
+    full: image18,
+  },
+  {
+    thumbnail: image19,
+    full: image19,
+  },
+  {
+    thumbnail: image20,
+    full: image20,
+  },
+  {
+    thumbnail: image21,
+    full: image21,
+  },
+  {
+    thumbnail: image22,
+    full: image22,
+  },
+  {
+    thumbnail: image23,
+    full: image23,
+  },
+  {
+    thumbnail: image24,
+    full: image24,
+  },
+  {
+    thumbnail: image25,
+    full: image25,
+  },
+  {
+    thumbnail: image26,
+    full: image26,
+  },
+  {
+    thumbnail: image27,
+    full: image27,
+  },
+  {
+    thumbnail: image28,
+    full: image28,
   },
 ];
